aaad5f659fb27d71b98028da1734e7f71b8f6ac0
saiyadss <95772136+saiyadss@users.noreply.github.com>
2022-05-04 12:45:08 -0700

    Add files via upload

---
+++ Project6_ternary_operators_constructors/JS/main.js
@@ -0,0 +1,7 @@
+
+function Ride_Function() {
+    var Height, Can_Ride;
+    Height = document.getElementById("Height").value;
+    Can_Ride = (Height < 52) ? "You are too short" : "You are tall enough";
+    document.getElementById("Ride").innerHTML = Can_Ride + " to ride.";
+}
---
+++ Project6_ternary_operators_constructors/JS/main1.js
@@ -0,0 +1,10 @@
+
+function count_Function() {
+    document.getElementById("Counting").innerHTML = Count();
+    function Count() {
+        var Starting_point = 9;
+        function Plus_one() {Starting_point += 1;}
+        Plus_one();
+        return Starting_point;
+    }
+}
